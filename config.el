d1f8cca135f0bf103f6c13db08647d349dca7ed3
Luis Victoria <v@lambda.lv>
2024-05-02 03:00:19 -0400

    Add lambda splashscreen

--- config.el
+++ config.el
@@ -74,3 +74,35 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;;; Splashscreen
+(defun splashscreen-lambda ()
+  (let* ((banner '("⠀⠀⠀⠀⠀⠀⢀⣤⣶⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⢸⣿⡿⠋⠻⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⡿⠀⠘⣿⣿⣷⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⠀⣾⣿⣿⣿⡿⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀"
+                   "⠀⠀⠀⠀⣾⣿⣿⣿⡟⠀⠀⠀⠀⢹⣿⣿⣷⠀⠀⠀⠀⠀"
+                   "⠀⠀⢀⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⣿⣿⣿⣷⡀⠀⠀⠀"
+                   "⠀⣠⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿"
+                   "⠈⢿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⠿⠁"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+      (point)
+      (dolist (line banner (point))
+        (insert (+doom-dashboard--center
+                  +doom-dashboard--width
+                  (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+                "\n"))
+      'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'splashscreen-lambda)
